430a30b1c6c10a18f64615080333b56515f1a88b
Hae-Riri <ehl3288@naver.com>
2022-03-10 15:22:55 +0900

    env: springboot setting

---
+++ src/main/java/com/example/springstudycode/SpringStudyCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springstudycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudyCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springstudycode/SpringStudyCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springstudycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
